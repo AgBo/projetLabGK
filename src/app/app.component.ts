ea6e3d6d0f317061e2a2ecc3bc919acb3a1569d4
Agnes Boitelle <aboitelle@hotmail.com>
2020-05-15 18:05:41 +0200

    avec erreur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'training-app';
+  // session_name = " Web2";
+  tittle = 'app'
+  nbpart: number = 0;
+  firstSession = {
+    id: 1,
+    name: 'Formation Web',
+    track: 'MEAN Stack',
+    date: new Date('2019-06-03'),
+    duree: 3,
+    local: 'Lyon',
+    participants: 0
+  };
+  nbrParticipantsChange(event) {
+    this.nbpart = event.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { SessionItComponent } from './session-it/session-it.component';
+import { SessionItemListComponent } from './session-item-list/session-item-list.component';
+import { InscriptiondisabledDirective } from './inscriptiondisabled.directive';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'list',
+    component: SessionItemListComponent
+  },
+  {
+    path: 'admin',
+    loadChildren: 'app/admin/admin.module#AdminModule'
+  },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SessionItComponent,
+    SessionItemListComponent,
+    InscriptiondisabledDirective,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes,{ enableTracing: true }),
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
